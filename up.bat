40fd72a97e8fea0b4c567bf328146d3662cba744
Hermes Passer <dsldouglas@hotmail.com>
2017-07-21 12:52:47 -0300

    Layout updated.

--- up.bat
+++
@@ -1,4 +0,0 @@
-python constructor.py
-git add -A
-git commit -m "Layout update."
-git push
---
+++ update.bat
@@ -0,0 +1,5 @@
+python constructor.py
+git add -A
+git commit -m "Layout updated."
+git push
+rmdir pages /s /q
