fb8d5d89973200c790d1d026310866de6dda550e
Hermes Passer <dsldouglas@hotmail.com>
2018-07-31 18:32:36 -0300

    fixed bugs on ramu demos

--- p/ramu/ramu-0.7b.js
+++ p/ramu/ramu-0.7b.js
@@ -174,8 +174,9 @@ Ramu._key = function () {
 Ramu._getMousePosition = function (event) {
 	var bound = Ramu.canvas.getBoundingClientRect();
 	return {
-		X: event.clientX - bound.left - Ramu.canvas.clientLeft,
-		Y: event.clientY - bound.top - Ramu.canvas.clientTop
+		// previously used bound.left/bound.top but it not work well when the canvas is distorced.
+		X: event.clientX - bound.x - Ramu.canvas.clientLeft,
+		Y: event.clientY - bound.y - Ramu.canvas.clientTop
 	};
 };
 
