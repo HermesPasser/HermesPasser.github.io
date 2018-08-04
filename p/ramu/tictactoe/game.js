991c4f7fbcc70f9f840ace0f07f896f6d0e3677b
Hermes Passer <dsldouglas@hotmail.com>
2018-08-03 21:06:50 -0300

    fix demo tictactoe dub

--- p/ramu/tictactoe/game.js
+++ p/ramu/tictactoe/game.js
@@ -77,7 +77,7 @@ var Game = function (_GameObj) {
 	}, {
 		key: 'checkVictory',
 		value: function checkVictory() {
-			var positions = [[0, 1, 2], [2, 3, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+			var positions = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
 
 			for (var i in positions) {
 				var pos = positions[i];
